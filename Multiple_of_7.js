0828534a0222f9dab061b632e324a9506308bddb
DhanasriSivakumar24 <dhanasrisivakumar245@gmail.com>
2024-08-15 00:59:29 +0530

    Day 3 Multiple_of_7_in_an_array

---
+++ Multiple_of_7.js
@@ -0,0 +1,14 @@
+function multiple(){
+
+    for( let arrElement of arr){
+
+        if( arrElement % 7 == 0 )
+            
+            console.log("First Multiple of 7 is: "+ arrElement);
+
+    }
+    
+}
+
+let arr = [19,28,5,67,92];
+multiple(arr);
